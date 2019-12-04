0b00a5399676d3c0fb706462335f0ef81b131745
letchepare <dev@lilian-etchepare.com>
2019-12-04 23:50:01 +0100

    Implemented login for api, api_token is bugged

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('api_token', 80)->unique()
+                                ->nullable()
+                                ->default(null);
+            $table->rememberToken();
         });
     }
 
