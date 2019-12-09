b578a1606e0742af80a32cdccbd3ca6c94763c8a
Lilian Etchepare <dev@lilian-etchepare.com>
2019-12-09 12:28:57 +0100

    Made exchange list beautifier
    Showing exchange list in a table

--- resources/views/exchanges/list.blade.php
+++ resources/views/exchanges/list.blade.php
@@ -2,30 +2,26 @@
 
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
+<div class="container">
+    
+    
+    <div class="row justify-content-center">
+            @if (count($exchanges) > 0)
+                @yield('list-grid')
+            @else
             <div class="col-md-10">
                 <div class="card">
                     <div class="card-header">Exchanges</div>
 
                     <div class="card-body">
-                        @if (count($exchanges) > 0)
-                            @foreach($exchanges as $exchange)
-                                <div class="alert alert-success" role="alert">
-                                    {{ $exchange->connexion_name }} <br>
-                                    {{ $exchange->api_key }} <br>
-                                    {{ $exchange->api_secret }} <br>
-                                    {{ $exchange->api_passphrase }}
-                                </div>
-                            @endforeach
-                        @else
                             <div class="alert alert-danger" role="alert">
                                 no exchange recorded on this account
                             </div>
-                        @endif
                     </div>
                 </div>
             </div>
+            @endif
+
         </div>
     </div>
 @endsection
